c1467327155ca24d78c864f11e2980a8ae647077
andbilous-digis <andbilous@gmail.com>
2020-05-09 01:27:17 +0300

    eslint

---
+++ .history/src/components/Modal_20200509010224.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import HyperModal from 'react-hyper-modal';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import {Row,Col} from 'reactstrap';
+
+
+const ModalComponent = ({classes,open,handleDrawerOpen,toggle,userDetails,closeModal}) => {
+  console.log('User Details',userDetails)
+  if(userDetails.orders){
+  return (
+    <Row>
+      <Col>
+      <HyperModal
+        isOpen={open}
+        requestClose={closeModal}
+        // requestClose={this.closeModal}
+      >
+         {userDetails.orders.length ? (
+          <Table className= size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Ціна</TableCell>
+            <TableCell>Валюта</TableCell>
+            <TableCell>Спосіб оплати</TableCell>
+            <TableCell>Статус</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {userDetails.orders.map(order=>{
+            return (
+          <TableRow>
+              <TableCell>{order.id}</TableCell>
+              <TableCell>{order.price}</TableCell>
+              <TableCell>{order.currency}</TableCell>
+              <TableCell>{order.currency}</TableCell>
+              <TableCell>{order.status}</TableCell>
+              <TableCell>4</TableCell>
+              <TableCell>
+              </TableCell>
+            </TableRow>
+            )
+            
+          })}
+        </TableBody>
+      </Table>
+         ):
+         <h1>Нет заказов</h1>}
+      </HyperModal>
+      </Col>
+    </Row>
+    
+)}else{
+  return (
+<div></div>
+  )
+}
+}
+export default ModalComponent;
---
+++ .history/src/pages/Users_20200509010735.js
@@ -0,0 +1,99 @@
+import React,{useState,useEffect} from 'react';
+import {connect} from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TableComponent from '../components/Table';
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import {fetchUsers,getUserData} from '../redux/users/user.actions';
+import {Row,Col} from 'reactstrap'
+import ModalComponent from '../components/Modal'
+import useStyles from './'
+
+
+
+
+const Users = (props)=> {
+  const classes = useStyles();
+  const [open, setOpen] = useState(true);
+    useEffect( ()=>{
+    props.fetchUsers();
+  },[])
+
+ 
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const [isModal, setIsModal] = useState(false)
+
+
+  const toggle = () => setOpen(!open);
+
+  const closeModal= ()=>setIsModal(false);
+
+  const handleClickOnOrderBtn=(id)=>{
+    props.getUserData(id)
+    setIsModal(true)
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header
+      classes={classes}
+      open={open}
+      handleDrawerOpen={handleDrawerOpen}
+      />
+      <Sidebar
+      classes={classes}
+      open={open}
+      handleDrawerClose={handleDrawerClose}
+      />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+       <ModalComponent closeModal={closeModal} userDetails={props.userData} open={isModal} toggle={toggle}/>
+              <Paper className={classes.paper}>
+                <TableComponent
+                users={props.users}
+                setUserId={handleClickOnOrderBtn}
+                />
+              </Paper>
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+          </Box>
+        </Container>
+      </main>
+    </div>
+  );
+}
+const UsersContainer = connect(
+  (state) => ({
+      users: state.usersReducer.users,
+      userData: state.usersReducer.userData
+  }), (dispatch) => ({
+    fetchUsers: () => dispatch(fetchUsers()),
+    getUserData: (id)=>dispatch(getUserData(id)) 
+  })
+)(Users);
+
+export {UsersContainer as Users} ;
+
+
+
+
+
+
